8954e7c500ab0e3cc7735edf65a72ec1d4d373fe
unknown <remcomooij@gmail.com>
2020-02-29 16:45:02 -0500

    added table to webpage

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+table = {}
+
+var cities = tableData.map((sighting) => {
+    return sighting.city;
+});
+console.log("Cities: ", cities); 
+
+data.forEach((sighting) => {
+    var row = d3.select("tbody").append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
