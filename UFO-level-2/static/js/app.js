98bd6ced66e6ae2dc989a1c9f1f6a75e9bc4a7a8
unknown <remcomooij@gmail.com>
2020-03-03 12:37:29 -0500

    finished level 2 of assignment

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -17,36 +17,50 @@ button.on("click", function() {
     d3.selectAll("tbody>tr").remove();
 
     var dateInput = d3.select("#datetime").property("value");
-    console.log(dateInput);
-
     var cityInput = d3.select("#city").property("value");
-    console.log(cityInput);
+    var stateInput = d3.select("#state").property("value");
+    var countryInput = d3.select("#country").property("value");
+    var shapeInput = d3.select("#shape").property("value");
 
-    // var stateInput = d3.select("#state").property("value");
-    // console.log(stateInput);
+    var filteredDate = tableData.filter(sighting => sighting.datetime === dateInput);
+    var filteredCity = tableData.filter(sighting => sighting.city === cityInput);
+    var filteredState = tableData.filter(sighting => sighting.state === stateInput);
+    var filteredCountry = tableData.filter(sighting => sighting.country === countryInput);
+    var filteredShape = tableData.filter(sighting => sighting.shape === shapeInput);
 
-    // var countryInput = d3.select("#country").property("value");
-    // console.log(countryInput);
+    filteredDate.forEach((sighting) => {
+        var row = d3.select("tbody").append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 
-    // var shapeInput = d3.select("#shape").property("value");
-    // console.log(shapeInput);
+    filteredCity.forEach((sighting) => {
+        var row = d3.select("tbody").append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 
-    var filteredByDate = tableData.filter(sighting =>
-        sighting.datetime.toLowerCase().includes(dateInput.toLowerCase()));
+    filteredState.forEach((sighting) => {
+        var row = d3.select("tbody").append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 
-    var filteredByCity = tableData.filter(sighting =>
-        sighting.city.toLowerCase().includes(cityInput.toLowerCase()));
+    filteredCountry.forEach((sighting) => {
+        var row = d3.select("tbody").append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 
-    // var filteredByState = tableData.filter(sighting =>
-    //     sighting.state.toLowerCase().includes(stateInput.toLowerCase()));
-
-    // var filteredByCountry = tableData.filter(sighting =>
-    //     sighting.country.toLowerCase().includes(countryInput.toLowerCase()));
-
-    // var filteredByShape = tableData.filter(sighting =>
-    //     sighting.shape.toLowerCase().includes(shapeInput.toLowerCase()));
-
-    filteredByDate.forEach((sighting) => {
+    filteredShape.forEach((sighting) => {
         var row = d3.select("tbody").append("tr");
         Object.entries(sighting).forEach(([key, value]) => {
             var cell = row.append("td");
@@ -54,38 +68,6 @@ button.on("click", function() {
         });
     });
     
-    filteredByCity.forEach((sighting) => {
-        var row = d3.select("tbody").append("tr");
-        Object.entries(sighting).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-
-    // filteredByState.forEach((sighting) => {
-    //     var row = d3.select("tbody").append("tr");
-    //     Object.entries(sighting).forEach(([key, value]) => {
-    //         var cell = row.append("td");
-    //         cell.text(value);
-    //     });
-    // });
-
-    // filteredByCountry.forEach((sighting) => {
-    //     var row = d3.select("tbody").append("tr");
-    //     Object.entries(sighting).forEach(([key, value]) => {
-    //         var cell = row.append("td");
-    //         cell.text(value);
-    //     });
-    // });
-
-    // filteredByShape.forEach((sighting) => {
-    //     var row = d3.select("tbody").append("tr");
-    //     Object.entries(sighting).forEach(([key, value]) => {
-    //         var cell = row.append("td");
-    //         cell.text(value);
-    //     });
-    // });
-
 });
 
 
